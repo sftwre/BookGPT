aa8539ac3f900bc8de46f2fa663187eb2f6b8adc
Isaac Buitrago <mightyking7@gmail.com>
2024-03-08 10:57:31 -0600

    Created DocWriter class to save book contents into a docx file

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from characters import MainCharacterChain
 from structure import get_structure
 from events import get_events
 from writing import write_book
+from publishing import DocWriter
 
 subject = "Machine War"
 author = "Ernest Hemingway"
@@ -11,9 +12,12 @@ genre = "horror"
 
 main_character_chain = MainCharacterChain()
 profile = main_character_chain.run('profile.pdf')
+doc_writer = DocWriter()
 
 title, plot, chapters_dict = get_structure(subject, genre, author, profile)
 summaries_dict, event_dict = get_events(subject, genre, author,
                                         profile, title, plot, chapters_dict)
 
 book = write_book(genre, author, title, profile, plot, summaries_dict, event_dict)
+
+doc_writer.write_doc(book, chapters_dict, title)
